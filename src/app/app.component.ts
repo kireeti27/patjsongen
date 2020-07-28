8b67cbc03f7b2927539302e51cbe7b7566da7bd3
revurikireeti <revurikireeti@gmail.com>
2020-07-28 10:52:30 +0530

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl,FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Demo';
+  
+  PatientDetails = new FormGroup({
+    Name: new FormControl(''),
+    MedicalRecordNumber : new FormControl(''),
+    FinancialNumber : new FormControl(''),
+    AdmitSource : new FormControl(''),
+    Organisation : new FormControl(''),
+    Building : new FormControl(''),
+    NurseUnit :  new FormControl(''),
+    Sex :  new FormControl(''),
+    Race :  new FormControl(''),
+    Age :  new FormControl(''),
+    EncounterStatus :  new FormControl(''),
+    EncounterType :  new FormControl('')
+  });
+
+  TransferDetails = new FormGroup({
+    MedicalService :  new FormControl(''),
+    Building :  new FormControl(''),
+    NurseUnit :  new FormControl(''),
+    RequestedAccommodation :  new FormControl(''),
+    TransferReason :  new FormControl('')
+  });
+ 
+  VitalsAndLabs = new FormGroup({
+    MAPInvasive : new FormControl(''),
+    MAPInvasiveSec : new FormControl(''),
+    MAPCuff : new FormControl(''),
+    MAPAssisted : new FormControl(''),
+    SBPNonInvasive : new FormControl(''),
+    SBPAssisted : new FormControl(''),
+    SBPInvasive : new FormControl(''),
+    SBPInvasiveSec : new FormControl(''),
+    SBPInvasivemmHg : new FormControl(''),
+    DBPNonInvasive : new FormControl(''),
+    DBPAssisted : new FormControl(''),
+    DBPInvasive : new FormControl(''),
+    DBPInvasivemmHg : new FormControl(''),
+    DBPInvasiveSec : new FormControl(''),
+    PulseRate : new FormControl(''),
+    ApicalHeartRate : new FormControl(''),
+    HRMonitored : new FormControl(''),
+    HeartRate : new FormControl(''),
+    HeartRatebpm : new FormControl(''),
+    PerpheralPulse : new FormControl(''),
+    RRTotal : new FormControl(''),
+    RR : new FormControl(''),
+    RRbrmin : new FormControl(''),
+    TempAux : new FormControl(''),
+    CoreTemp : new FormControl(''),
+    TempOral : new FormControl(''),
+    TempRectal : new FormControl(''),
+    TempRectalF : new FormControl(''),
+    TempTympanic : new FormControl(''),
+    HCTPOC : new FormControl(''),
+    PlateletCount : new FormControl(''),
+    SodiumLevel : new FormControl('')
+  })
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms'
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
